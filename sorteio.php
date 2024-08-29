e4c9144ccd942becb8d1c3718c1655cd68132fb3
Ruanpedr <ruanpedrorp6@gmail.com>
2024-08-29 20:12:06 -0300

    Add files via upload

---
+++ sorteio.php
@@ -0,0 +1,27 @@
+<?php
+
+$numero =5; 
+
+switch ($numero) {
+    case 1:
+        $mensagem = "uma bicicleta";
+        break;
+    case 2:
+        $mensagem = "20 mil reais em barras de ouro";
+        break;
+    case 3:
+        $mensagem = "uma casa";
+        break;
+    case 4:
+        $mensagem = "um conjunto de panelas";
+        break;
+    case 5:
+        $mensagem = "um carro zero";
+        break;
+    default:
+        $mensagem = "nada, infelizmente";
+        break;
+}
+
+echo "Parabéns, o seu prêmio foi: $mensagem.";
+?>
