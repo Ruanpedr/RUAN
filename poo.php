ad0585c8058cefa3861dd7ceac900e7899493b25
Ruanpedr <ruanpedrorp6@gmail.com>
2024-09-09 19:32:16 -0300

    Add files via upload

---
+++ poo.php
@@ -0,0 +1,35 @@
+<?php>
+<body>
+
+class fruta{
+    //
+puiblic $name;
+public $colo;
+
+function set_name($name){
+    $this->name = ¨$name;  
+}
+
+function get_name(){
+  return $this->name;
+
+}
+function get_name(){
+  return $this->name;
+  
+}  
+
+}
+
+$apple = new fruta();
+$banana= new fruta();
+$apple-> set_name('apple')
+$banana-> set_name('banana')
+
+echo $apple->get_name();
+echo <br>;
+echo $banana->get_name();
+
+?>
+</html>
+</body> 	                
